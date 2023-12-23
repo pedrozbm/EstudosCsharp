964141243417e422d7a0e239402a5c27989b3bec
Pedro Barbieri <112345954+pedrozbm@users.noreply.github.com>
2023-12-23 14:17:48 -0400

    Add files via upload

---
+++ ConversaoImplicitaCasting/ConversaoImplicitaCasting/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ConversaoImplicitaCasting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Conversão  implicita entre tipos é quando um conteúdo de um certo tipo pode ser naturalmente
+            // atribuído para uma variável de outro tipo
+            // Casting é uma conversão explicita entre tipos compatíveis
+
+            float x = 4.5f;
+            double y = x;
+            // Esta é uma conversão implicita
+
+            double a;
+            float b;
+            a = 5.1;
+            b = (float)a;
+            //Esse é um exemplo de Cast
+
+            Console.WriteLine(b);
+            //Para divisoões de números inteiros´, é importante lembrar do cast
+            int n1 = 5;
+            int n2 = 2;
+
+            Console.WriteLine(n1 / n2);// Gera um resultado inteiro
+            double resultado = (double) n1 / n2;
+            Console.WriteLine(resultado);
+        }
+    }
+} 
---
+++ Funcoes/Funcoes/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+// Debugging: 
+//Teclas
+//• F9 - marcar/desmarcar breakpoint
+//• F5 - iniciar/continuar o debug
+//• F10 - executar um passo (pula função)
+//• F11 - executar um passo(entra na função)
+//• SHIFT + F11 - sair do método em execução
+//• SHIFT + F5 - parar debug
+//• Janelas
+//• Watch (expressões personalizadas)
+//• Autos(expressões "interessantes" detectadas pelo Visual Studio)
+//• Locals(variáveis locais)
+
+
+namespace Funcoes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Funções representam um processamento que possui um significado
+            // Em orientação a objetos, funções em classe recebem o nome de "Metodos"
+
+            Console.WriteLine("Digite 3 numeros");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine()); 
+            int n3 = int.Parse(Console.ReadLine());
+
+           double resultado = Maior(n1, n2, n3);
+
+            Console.WriteLine("Maior: "+ resultado);
+        }
+        static int Maior(int n1,int n2,int n3) {
+                int m;
+
+                if (n1 > n2 && n1 > n3)
+                {
+                    m = n1;
+                }
+                else if (n2 > n3 && n2 > n3)
+                {
+                    m = n2;
+                }
+                else
+                {
+                    m = n3;
+                }
+                return m;
+            }
+
+    }
+}
