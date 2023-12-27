a8f5b98d754950191095ec905f87fc521f58a7d6
Pedro Barbieri <112345954+pedrozbm@users.noreply.github.com>
2023-12-27 18:14:56 -0400

    Add files via upload
    Exercício de fixação

---
+++ ExercicioFixacaoEncapsulamento/ContaBancaria.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ExercicioFixacaoEncapsulamento
+{
+    internal class ContaBancaria
+    {
+        private double _saldo;
+        public string _nome { get; private set; }
+        public int _numeroConta { get; private set; }
+
+        public ContaBancaria()
+        {
+
+        }
+        public ContaBancaria(string nome, int numero, double saldo)
+        {
+            _numeroConta = numero;
+            _nome = nome;
+            _saldo = saldo;
+        }
+        public void Deposito(double deposito)
+        {
+            Saldo += deposito;
+        }
+        public void Saque(double valor)
+        {
+            valor = valor + 5;
+            Saldo -= valor;
+        }
+        public double Saldo
+        {
+            get { return _saldo; }
+            set { _saldo = value; }
+        }
+        public int Nconta
+        {
+            set { _numeroConta = value; }
+        }
+        public void Nome(string nome)
+        {
+            _nome = nome;
+        }
+        public override string ToString()
+        {
+            return "Conta " + _numeroConta
+            + ", Titular: "
+            + _nome
+            + ", Saldo: $ "
+            + _saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ ExercicioFixacaoEncapsulamento/Program.cs
@@ -0,0 +1,66 @@
+﻿/*
+ * 
+ * Exercício:
+ * 
+Em um banco, para se cadastrar uma conta bancária, é necessário informar o número da conta, o nome do 
+titular da conta, e o valor de depósito inicial que o titular depositou ao abrir a conta. Este valor de depósito 
+inicial, entretanto, é opcional, ou seja: se o titular não tiver dinheiro a depositar no momento de abrir sua 
+conta, o depósito inicial não será feito e o saldo inicial da conta será, naturalmente, zero.
+Importante: uma vez que uma conta bancária foi aberta, o número da conta nunca poderá ser alterado. Já 
+o nome do titular pode ser alterado (pois uma pessoa pode mudar de nome por ocasião de casamento, por 
+exemplo). 
+Por fim, o saldo da conta não pode ser alterado livremente. É preciso haver um mecanismo para proteger 
+isso. O saldo só aumenta por meio de depósitos, e só diminui por meio de saques. Para cada saque 
+realizado, o banco cobra uma taxa de $ 5.00. Nota: a conta pode ficar com saldo negativo se o saldo não for 
+suficiente para realizar o saque e/ou pagar a taxa.
+Você deve fazer um programa que realize o cadastro de uma conta, dando opção para que seja ou não 
+informado o valor de depósito inicial. Em seguida, realizar um depósito e depois um saque, sempre 
+mostrando os dados da conta após cada operação
+*/
+using System.Runtime.Intrinsics.X86;
+
+using System.Globalization;
+
+
+namespace ExercicioFixacaoEncapsulamento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta = new ContaBancaria();
+
+
+
+            Console.WriteLine(conta.Saldo);
+            Console.WriteLine("Entre com o número da conta! ");
+            conta.Nconta = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com o Titular da conta: ");
+            conta.Nome(Console.ReadLine());
+
+            Console.WriteLine("Haverá deposito inicial? (s/n)");
+            string resp = Console.ReadLine();
+            if(resp == "s" || resp == "S")
+            {
+                Console.WriteLine("Entre o valor para depósito: ");
+                conta.Deposito(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            }
+
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine("Entre um valor para deposito: ");
+            conta.Deposito(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine("Entre um valor para saque: ");
+            conta.Saque(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+        }
+    }
+}
