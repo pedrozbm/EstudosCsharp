b9cfcafb37623c6998652da7dcea80a8ec745cb0
Pedro Barbieri <112345954+pedrozbm@users.noreply.github.com>
2024-02-08 18:41:41 -0300

    Add files via upload

---
+++ LaçoForeach/LaçoForeach/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace LaçoForeach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vect = new string[] { "Maria", "Bob", "Alex" };
+
+            foreach (var ve in vect)
+            {
+                Console.WriteLine(ve);
+            }
+        }
+    }
+}
