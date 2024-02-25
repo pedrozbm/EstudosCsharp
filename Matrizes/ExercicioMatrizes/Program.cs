51308f29f6e385c91c57c3cd466e83ca480dec20
Pedro Barbieri <112345954+pedrozbm@users.noreply.github.com>
2024-02-25 18:09:27 -0300

    Add files via upload

---
+++ Matrizes/ExercicioMatrizes/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Runtime.Intrinsics.X86;
+namespace ExercicioMatrizes
+
+//Fazer um programa para ler dois números inteiros M e N, e depois ler
+//uma matriz de M linhas por N colunas contendo números inteiros,
+//podendo haver repetições.Em seguida, ler um número inteiro X que
+//pertence à matriz.Para cada ocorrência de X, mostrar os valores à
+//esquerda, acima, à direita e abaixo de X, quando houver, conforme
+//exemplo
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int m = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[m, n];
+
+            for (int i = 0; i < m; i++)
+            {
+                string[] values = Console.ReadLine().Split(" ");
+
+                for (int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+            Console.WriteLine("Numero procurado: ");
+            int searchNum = int.Parse(Console.ReadLine());
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i, j] == searchNum)
+                    {
+                        Console.WriteLine("Position " + i + "," + j + ":");
+                        if (j > 0)
+                        {
+                            Console.WriteLine("Left: " + mat[i, j - 1]);
+                        }
+                        if (i > 0)
+                        {
+                            Console.WriteLine("Up: " + mat[i - 1, j]);
+                        }
+                        if (j < n - 1)
+                        {
+                            Console.WriteLine("Right: " + mat[i, j + 1]);
+                        }
+                        if (i < m - 1)
+                        {
+                            Console.WriteLine("Down: " + mat[i + 1, j]);
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ Matrizes/Matrizes/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace Matrizes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //criando matriz:
+            double[,] matt = new double[2, 3];
+
+            //Console.WriteLine(matt.Length);
+            //Console.WriteLine(matt.Rank);
+            //Console.WriteLine(matt.GetLength(0));
+            //Console.WriteLine(matt.GetLength(1));
+
+
+            //Fazer um programa para ler um número inteiro N
+            //e uma matriz de ordem N cotendo números inteiros. Em seguida
+            //mostrar a diagonal principal e a quantidade de valores negativos
+            //da matriz
+
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+            Console.WriteLine("Main diagonal: ");
+            for(int i = 0; i <n; i++) {
+                Console.WriteLine(mat[i, i]);
+            }
+            int count = 0;
+            for(int i = 0; i<n; i++)
+            {
+                for(int j = 0;j < n; j++)
+                {
+                    if (mat[i, j]<0)
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine("Negative numbers: "+count);
+            
+        }
+    }
+}
